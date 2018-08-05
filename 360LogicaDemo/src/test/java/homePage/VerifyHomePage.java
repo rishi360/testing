3dac89a50e25a89c6f629b6abb998b4f04527f21
rishi360 <rishi_caprion@yahoo.com>
2018-08-05 10:09:36 +0530

    Update VerifyHomePage.java

--- 360LogicaDemo/src/test/java/homePage/VerifyHomePage.java
+++ 360LogicaDemo/src/test/java/homePage/VerifyHomePage.java
@@ -34,7 +34,7 @@ public class VerifyHomePage {
 	public void testVerifyHomePage() {
 
 		// launch the application
-		driver.get("http://192.168.9.21:31198/");
+		driver.get("http://stage.360logica.gq:30000/");
 		
 		wait= new WebDriverWait(driver,30);
 
